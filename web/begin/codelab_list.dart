db9f67b9437844cc693e4b6ac16c0797b01f4efb
Harry Terkelsen <het@google.com>
2015-04-20 18:43:54 -0700

    Revert "Done with step 5"
    This reverts commit 8bb17a538911132c62387614d3f3f04effc6862c.

--- web/begin/codelab_list.dart
+++ web/begin/codelab_list.dart
@@ -4,10 +4,5 @@ import 'dart:html' show Event, Node;
 
 @CustomTag('codelab-list')
 class CodelabList extends PolymerElement {
-  @observable Codelab newCodelab = new Codelab();
-  String get defaultLevel => Codelab.LEVELS[1];
-
-  CodelabList.created() : super.created() {
-    newCodelab.level = defaultLevel;
-  }
+  CodelabList.created() : super.created() {}
 }
--- web/begin/model.dart
+++ web/begin/model.dart
@@ -2,16 +2,4 @@ library polymer_and_dart.web.models;
 
 import 'package:polymer/polymer.dart';
 
-class Codelab extends Observable {
-  static const List<String> LEVELS =
-      const ['easy', 'intermediate', 'advanced'];
-  static const MIN_TITLE_LENGTH = 10;
-  static const MAX_TITLE_LENGTH = 30;
-  static const MAX_DESCRIPTION_LENGTH = 140;
-
-  @observable String title;
-  @observable String description;
-  @observable String level;
-
-  Codelab([this.title = "", this.description = ""]);
-}
+class Codelab extends Observable {}
